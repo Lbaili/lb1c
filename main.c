98a435083d2d83a7be4820a37b0ece8e498eb401
Chang Li <1295118952@qq.com>
2020-09-01 14:42:39 +0000

    .

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 
 int main(void) {
 char *ctemp=readline("Enter temperature in celsius: " );
-double Tem= atof(ctemp);
-double Fah=Tem*1.8+32.0;  
-printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",Tem, Fah);
+double c= atof(ctemp);
+double f=c*1.8+32.0;  
+printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",c, f);
 
 return 0;
 }

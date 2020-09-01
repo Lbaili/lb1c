755a22a78983b4bb4ac68d5bcd6665e5aa05e99b
Chang Li <1295118952@qq.com>
2020-09-01 14:26:12 +0000

    .

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include<readline/readline.h>
 # Author: Yanling Wang yuw17@psu.edu
-# Collaborator: cxl5844@psu.edu
+# Collaborator: cxl5844@psu.edu#include <stdio.h>
+#include<readline/readline.h>
+
 
 
 int main(void) {
-double Tem=(double) readline("Enter temperature in celsius: ");
+double Tem=0.0;
+Tem= readline("Enter temperature in celsius: " );
 double Fah=0.0;
 Fah=Tem*1.8+32.0;
 printf(Tem + "° in Celsius is equivalent to "+Fah +"° Fahrenheit.");
